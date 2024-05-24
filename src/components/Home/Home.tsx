147509088a8bf6698e8b639c1f92f43a4eec1deb
Bisyk <bskyrslv@gmail.com>
2024-05-24 19:13:38 +0300

    added text to home screen if there is no quizes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -11,6 +11,9 @@ const Home = () => {
     dispatch(quizesActions.deleteQuiz({ title: title }));
   };
 
+  console.log(quizes);
+  console.log(quizes.length);
+
   return (
     <div>
       <p className="flex justify-between items-center px-4 mt-2 mb-2">
@@ -21,28 +24,41 @@ const Home = () => {
       </p>
       <hr></hr>
       <div className="">
-        {quizes.map((quiz: QuizType) => (
-          <div className="flex w-full px-2" key={quiz.title}>
-            <div className="w-[94%]">
-              <Link to={`/quiz/${quiz.title}`} key={quiz.title}>
-                <div className="flex justify-between p-4 mr-2 border border-gray-400 rounded-xl my-2 cursor-pointer hover:bg-slate-200 duration-300">
-                  <h2>{quiz.title}</h2>
-                </div>
-              </Link>
+        {quizes.length > 0 ? (
+          quizes.map((quiz: QuizType) => (
+            <div className="flex w-full px-2" key={quiz.title}>
+              <div className="w-[94%]">
+                <Link to={`/quiz/${quiz.title}`} key={quiz.title}>
+                  <div className="flex justify-between p-4 mr-2 border border-gray-400 rounded-xl my-2 cursor-pointer hover:bg-slate-200 duration-300">
+                    <h2>{quiz.title}</h2>
+                  </div>
+                </Link>
+              </div>
+              <div className="flex gap-4">
+                <Link to={`/quiz/${quiz.title}/edit`} className="flex">
+                  <button className="text-blue-400 font-semibold">Edit</button>
+                </Link>
+                <button
+                  onClick={() => handleDeleteQuiz(quiz.title)}
+                  className="text-rose-400 font-semibold"
+                >
+                  Delete
+                </button>
+              </div>
             </div>
-            <div className="flex gap-4">
-              <Link to={`/quiz/${quiz.title}/edit`} className="flex">
-                <button className="text-blue-400 font-semibold">Edit</button>
+          ))
+        ) : (
+          <div className="flex flex-col items-center mt-20 gap-5">
+            <p className="text-3xl">No quizes found. Please create a new one!</p>
+            <p>
+              <Link to="/quiz/new">
+                <button className="bg-blue-400 px-4 py-2 rounded-xl">
+                  New Quiz
+                </button>
               </Link>
-              <button
-                onClick={() => handleDeleteQuiz(quiz.title)}
-                className="text-rose-400 font-semibold"
-              >
-                Delete
-              </button>
-            </div>
+            </p>
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
